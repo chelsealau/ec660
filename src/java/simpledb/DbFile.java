b68d48eac532da6460759ca393b39dbe1d9e850e
Cameron Cipriano <cameron.ciprianom@gmail.com>
2022-10-04 11:58:00 -0400

    Catalog fixes

--- src/java/simpledb/DbFile.java
+++ src/java/simpledb/DbFile.java
@@ -7,7 +7,7 @@ import java.io.*;
 /**
  * The interface for database files on disk. Each table is represented by a
  * single DbFile. DbFiles can fetch pages and iterate through tuples. Each
- * file has a unique id used to store metadata about the table in the Catalog.
+ * file has a unique id used to store metadata about thπe table in the Catalog.
  * DbFiles are generally accessed through the buffer pool, rather than directly
  * by operators.
  */
@@ -22,7 +22,8 @@ public interface DbFile {
     /**
      * Push the specified page to disk.
      *
-     * @param p The page to write.  page.getId().pageno() specifies the offset into the file where the page should be written.
+     * @param p The page to write. page.getId().pageno() specifies the offset into
+     *          the file where the page should be written.
      * @throws IOException if the write fails
      *
      */
@@ -34,14 +35,14 @@ public interface DbFile {
      * may block until the lock can be acquired.
      *
      * @param tid The transaction performing the update
-     * @param t The tuple to add.  This tuple should be updated to reflect that
-     *          it is now stored in this file.
+     * @param t   The tuple to add. This tuple should be updated to reflect that
+     *            it is now stored in this file.
      * @return An ArrayList contain the pages that were modified
      * @throws DbException if the tuple cannot be added
      * @throws IOException if the needed file can't be read/written
      */
     public ArrayList<Page> insertTuple(TransactionId tid, Tuple t)
-        throws DbException, IOException, TransactionAbortedException;
+            throws DbException, IOException, TransactionAbortedException;
 
     /**
      * Removes the specified tuple from the file on behalf of the specified
@@ -50,14 +51,14 @@ public interface DbFile {
      * may block until the lock can be acquired.
      *
      * @param tid The transaction performing the update
-     * @param t The tuple to delete.  This tuple should be updated to reflect that
-     *          it is no longer stored on any page.
+     * @param t   The tuple to delete. This tuple should be updated to reflect that
+     *            it is no longer stored on any page.
      * @return An ArrayList contain the pages that were modified
      * @throws DbException if the tuple cannot be deleted or is not a member
-     *   of the file
+     *                     of the file
      */
     public ArrayList<Page> deleteTuple(TransactionId tid, Tuple t)
-        throws DbException, IOException, TransactionAbortedException;
+            throws DbException, IOException, TransactionAbortedException;
 
     /**
      * Returns an iterator over all the tuples stored in this DbFile. The
@@ -73,7 +74,7 @@ public interface DbFile {
      * can be used to look up the table via {@link Catalog#getDatabaseFile} and
      * {@link Catalog#getTupleDesc}.
      * <p>
-     * Implementation note:  you will need to generate this tableid somewhere,
+     * Implementation note: you will need to generate this tableid somewhere,
      * ensure that each HeapFile has a "unique id," and that you always
      * return the same value for a particular HeapFile. A simple implementation
      * is to use the hash code of the absolute path of the file underlying
@@ -82,9 +83,10 @@ public interface DbFile {
      * @return an ID uniquely identifying this HeapFile.
      */
     public int getId();
-    
+
     /**
      * Returns the TupleDesc of the table stored in this DbFile.
+     * 
      * @return TupleDesc of this DbFile.
      */
     public TupleDesc getTupleDesc();
